2219671f3cae2929ecf03713aafb6a3aae791f83
HyeokJoon Kong <orijoon98@gmail.com>
2022-06-15 13:23:40 +0900

    [FIX] JPA 수정

--- src/main/java/com/kale/repository/UserRepository.java
+++ src/main/java/com/kale/repository/UserRepository.java
@@ -2,16 +2,11 @@ package com.kale.repository;
 
 import com.kale.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-
-    @Query("SELECT count(u) FROM USER u WHERE u.email = :email")
-    boolean existsByEmail(@Param("email") String email);
-
+    boolean existsByEmail(String email);
 }
