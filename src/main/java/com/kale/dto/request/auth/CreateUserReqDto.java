ce94a1f42b8e168d2311c949df638804d0a6b248
HyeokJoon Kong <orijoon98@gmail.com>
2022-06-15 13:36:12 +0900

    [FIX] dto validation 수정

--- src/main/java/com/kale/dto/request/auth/CreateUserReqDto.java
+++ src/main/java/com/kale/dto/request/auth/CreateUserReqDto.java
@@ -1,31 +1,21 @@
 package com.kale.dto.request.auth;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class CreateUserReqDto {
 
-
-    @Email
-    @NotBlank(message = "이메일은 필수 입력값입니다.")
-    @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}",
-            message = "올바르지 않은 이메일 형식입니다." )
+    @Email(message = "올바르지 않은 이메일 형식입니다.")
+    @NotNull(message = "이메일은 필수 입력값입니다.")
     private String email;
 
-    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @Pattern(regexp="^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{6,15}",
             message = "비밀번호는 영문과 숫자 조합으로 6~15자리까지 가능합니다")
     private String password;
-
-
 }
