5cd348a1e1b79413f141bd8d496d98743f2c717a
HyeokJoon Kong <orijoon98@gmail.com>
2022-06-15 14:02:30 +0900

    [FIX] dto validation message 수정

--- src/main/java/com/kale/dto/request/auth/CreateUserReqDto.java
+++ src/main/java/com/kale/dto/request/auth/CreateUserReqDto.java
@@ -16,6 +16,6 @@ public class CreateUserReqDto {
     private String email;
 
     @Pattern(regexp="^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{6,15}",
-            message = "비밀번호는 영문과 숫자 조합으로 6~15자리까지 가능합니다")
+            message = "비밀번호는 영문과 숫자 조합으로 6~15자리까지 가능합니다.")
     private String password;
 }
