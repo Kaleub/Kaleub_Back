800d0fd8f74207b2431b0643aa27f9eef7f2618b
heyazoo1007 <heyazoo1007@gmail.com>
2022-06-18 12:41:53 +0900

    [FEAT] 사용자 강퇴 기능 추가

---
+++ src/main/java/com/kale/dto/request/room/DeleteUserForceReqDto.java
@@ -0,0 +1,14 @@
+package com.kale.dto.request.room;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class DeleteUserForceReqDto {
+
+    private Long roomId;
+    private String deletedUserEmail;
+
+
+}
