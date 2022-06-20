9949b596b8622806b6cc54de7afffff2954be95b
HyeokJoon Kong <orijoon98@gmail.com>
2022-06-20 17:57:37 +0900

    [TEST] createRoom 성공 테스트

--- src/main/java/com/kale/dto/request/room/CreateRoomReqDto.java
+++ src/main/java/com/kale/dto/request/room/CreateRoomReqDto.java
@@ -1,5 +1,6 @@
 package com.kale.dto.request.room;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,4 +17,10 @@ public class CreateRoomReqDto {
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{6,15}",
             message = "비밀번호는 영문과 숫자 조합으로 6~15자리까지 가능합니다.")
     private String password;
+
+    @Builder(builderMethodName = "testBuilder")
+    public CreateRoomReqDto(String title, String password) {
+        this.title = title;
+        this.password = password;
+    }
 }
--- src/main/java/com/kale/repository/RoomRepository.java
+++ src/main/java/com/kale/repository/RoomRepository.java
@@ -1,6 +1,7 @@
 package com.kale.repository;
 
 import com.kale.domain.Room;
+import com.kale.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
     Optional<Room> findByCode(String code);
+    Optional<Room> findByOwnerUser(User user);
 }
