9bfe3687c7235185500994124833bcca3cfdee2e
HyeokJoon Kong <orijoon98@gmail.com>
2022-06-21 21:12:54 +0900

    [FEAT] S3 설정

---
+++ src/main/java/com/kale/exception/ImageUploadFailedException.java
@@ -0,0 +1,10 @@
+package com.kale.exception;
+
+public class ImageUploadFailedException extends RuntimeException {
+
+    private static final String MESSAGE = "파일 업로드에 실패했습니다.";
+
+    public ImageUploadFailedException() {
+        super(MESSAGE);
+    }
+}
