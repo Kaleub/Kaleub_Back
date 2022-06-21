4bd7f6aeb90a5c4d77847960db7a30449ff47879
HyeokJoon Kong <orijoon98@gmail.com>
2022-06-21 15:03:14 +0900

    [FEAT] date converter

---
+++ src/main/java/com/kale/util/DateUtil.java
@@ -0,0 +1,18 @@
+package com.kale.util;
+
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Service
+public class DateUtil {
+
+    public static long convertToTimeInterval(LocalDateTime createdTime) {
+
+        Timestamp timestamp = Timestamp.valueOf(createdTime);
+        long createdTimeInterval = timestamp.getTime() / 1000L;
+
+        return createdTimeInterval;
+    }
+}
