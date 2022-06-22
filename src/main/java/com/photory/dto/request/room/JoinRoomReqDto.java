42a25b8274256727b4146328996ab9c5d3f48dda
HyeokJoon Kong <orijoon98@gmail.com>
2022-06-22 12:10:51 +0900

    [TEST] 방 참가 성공 케이스 테스트

--- src/main/java/com/photory/dto/request/room/JoinRoomReqDto.java
+++ src/main/java/com/photory/dto/request/room/JoinRoomReqDto.java
@@ -1,5 +1,6 @@
 package com.photory.dto.request.room;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,4 +10,10 @@ public class JoinRoomReqDto {
 
     private String code;
     private String password;
+
+    @Builder(builderMethodName = "testBuilder")
+    public JoinRoomReqDto(String code, String password) {
+        this.code = code;
+        this.password = password;
+    }
 }
--- src/test/java/com/photory/service/RoomServiceTest.java
+++ src/test/java/com/photory/service/RoomServiceTest.java
@@ -5,10 +5,12 @@ import com.photory.domain.Participate;
 import com.photory.domain.Room;
 import com.photory.domain.User;
 import com.photory.dto.request.room.CreateRoomReqDto;
+import com.photory.dto.request.room.JoinRoomReqDto;
 import com.photory.repository.ParticipateRepository;
 import com.photory.repository.RoomRepository;
 import com.photory.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,6 +43,7 @@ public class RoomServiceTest {
     }
 
     @Test
+    @DisplayName("createRoomTest_성공")
     void createRoomTest_성공() {
         //given
         User user = User.builder()
@@ -66,13 +69,51 @@ public class RoomServiceTest {
                 () -> assertTrue(room.isPresent()),
                 () -> assertTrue(participate.isPresent()),
                 () -> assertEquals(saved.getId(), participate.get().getUser().getId()),
-                () -> assertEquals(saved.getEmail(), participate.get().getUser().getEmail()),
-                () -> assertEquals(saved.getPassword(), participate.get().getUser().getPassword()),
-                () -> assertEquals(saved.getRole(), participate.get().getUser().getRole()),
                 () -> assertEquals(room.get().getId(), participate.get().getRoom().getId()),
-                () -> assertEquals(room.get().getTitle(), participate.get().getRoom().getTitle()),
-                () -> assertEquals(room.get().getPassword(), participate.get().getRoom().getPassword()),
                 () -> assertEquals(room.get().getOwnerUser().getId(), participate.get().getRoom().getOwnerUser().getId())
+        );
+    }
+
+    @Test
+    @DisplayName("joinRoomTest_성공")
+    void joinRoomTest_성공() {
+        //given
+        User user1 = User.builder()
+                .email("user1@gmail.com")
+                .password("password1")
+                .role(Role.ROLE_USER)
+                .build();
+        User user2 = User.builder()
+                .email("user2@gmail.com")
+                .password("password1")
+                .role(Role.ROLE_USER)
+                .build();
+        User roomOwner = userRepository.save(user1);
+        User notOwner = userRepository.save(user2);
+
+        CreateRoomReqDto createRoomReqDto = CreateRoomReqDto.testBuilder()
+                .title("room")
+                .password("password1")
+                .build();
+        roomService.createRoom(roomOwner.getEmail(), createRoomReqDto);
+        Optional<Room> room = roomRepository.findByOwnerUser(roomOwner);
+
+        JoinRoomReqDto joinRoomReqDto = JoinRoomReqDto.testBuilder()
+                .code(room.get().getCode())
+                .password("password1")
+                .build();
+
+        //when
+        roomService.joinRoom(notOwner.getEmail(), joinRoomReqDto);
+
+        //then
+        Optional<Room> joinedRoom = roomRepository.findByOwnerUser(roomOwner);
+        Optional<Participate> participate = participateRepository.findByRoomAndUser(joinedRoom.get(), notOwner);
+
+        assertAll(
+                () -> assertTrue(participate.isPresent()),
+                () -> assertEquals(2, joinedRoom.get().getParticipantsCount()),
+                () -> assertEquals(participate.get().getRoom().getId(), joinedRoom.get().getId())
         );
     }
 }
