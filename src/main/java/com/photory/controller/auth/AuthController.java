54b03bc6d5be00d53b78feaff2475be3985f577e
HyeokJoon Kong <orijoon98@gmail.com>
2022-06-23 10:23:46 +0900

    [RENAME] dto 변수 이름 변경

--- src/main/java/com/photory/controller/auth/AuthController.java
+++ src/main/java/com/photory/controller/auth/AuthController.java
@@ -17,35 +17,35 @@ public class AuthController {
   
     //이메일이 유효한거 확인 되면, 인증 버튼 누를 수 있음
     @PostMapping("/signup/email/check")
-    public ApiResponse<String> validateEmail(@RequestBody @Valid ValidateEmailRequestDto validateEmailRequestDto) {
-        authService.validateEmail(validateEmailRequestDto);
+    public ApiResponse<String> validateEmail(@RequestBody @Valid ValidateEmailRequestDto request) {
+        authService.validateEmail(request);
         return ApiResponse.SUCCESS;
     }
 
     //이메일 인증 실행
     @PostMapping("/signup/email")
-    public ApiResponse<String> authEmail(@RequestBody @Valid AuthEmailRequestDto authEmailRequestDto) {
-        authService.authEmail(authEmailRequestDto);
+    public ApiResponse<String> authEmail(@RequestBody @Valid AuthEmailRequestDto request) {
+        authService.authEmail(request);
         return ApiResponse.SUCCESS;
     }
 
     @PostMapping("/signup/email/complete")
-    public ApiResponse<String> authEmailComplete(@RequestBody @Valid AuthEmailCompleteRequestDto authEmailRequestDto) {
-        authService.authEmailComplete(authEmailRequestDto);
+    public ApiResponse<String> authEmailComplete(@RequestBody @Valid AuthEmailCompleteRequestDto request) {
+        authService.authEmailComplete(request);
         return ApiResponse.SUCCESS;
     }
 
 
     //마지막으로 회원 생성
     @PostMapping("/signup")
-    public ApiResponse<String> createUser(@RequestBody @Valid CreateUserRequestDto createUserRequestDto) {
-        authService.createUser(createUserRequestDto);
+    public ApiResponse<String> createUser(@RequestBody @Valid CreateUserRequestDto request) {
+        authService.createUser(request);
         return ApiResponse.SUCCESS;
     }
 
     @PostMapping("/signin")
-    public ApiResponse<String> signinUser(@RequestBody SigninUserRequestDto signinUserRequestDto) {
-        String token = authService.signinUser(signinUserRequestDto);
+    public ApiResponse<String> signinUser(@RequestBody SigninUserRequestDto request) {
+        String token = authService.signinUser(request);
         return ApiResponse.success(token);
     }
 
--- src/main/java/com/photory/controller/room/RoomController.java
+++ src/main/java/com/photory/controller/room/RoomController.java
@@ -21,14 +21,14 @@ public class RoomController {
     private final RoomService roomService;
 
     @PostMapping
-    public ApiResponse<CreateRoomResponse> createRoom(@RequestBody @Valid CreateRoomRequestDto createRoomRequestDto, @UserEmail String userEmail) {
-        CreateRoomResponse createRoomResponse = roomService.createRoom(userEmail, createRoomRequestDto);
+    public ApiResponse<CreateRoomResponse> createRoom(@RequestBody @Valid CreateRoomRequestDto request, @UserEmail String userEmail) {
+        CreateRoomResponse createRoomResponse = roomService.createRoom(userEmail, request);
         return ApiResponse.success(createRoomResponse);
     }
 
     @PostMapping("/participate")
-    public ApiResponse<JoinRoomResponse> joinRoom(@RequestBody JoinRoomRequestDto joinRoomRequestDto, @UserEmail String userEmail) {
-        JoinRoomResponse joinRoomResponse = roomService.joinRoom(userEmail, joinRoomRequestDto);
+    public ApiResponse<JoinRoomResponse> joinRoom(@RequestBody JoinRoomRequestDto request, @UserEmail String userEmail) {
+        JoinRoomResponse joinRoomResponse = roomService.joinRoom(userEmail, request);
         return ApiResponse.success(joinRoomResponse);
     }
 
@@ -39,32 +39,32 @@ public class RoomController {
     }
 
     @DeleteMapping("/participate")
-    public ApiResponse<String> leaveRoom(@RequestBody LeaveRoomRequestDto leaveRoomRequestDto, @UserEmail String userEmail) {
-        roomService.leaveRoom(userEmail, leaveRoomRequestDto);
+    public ApiResponse<String> leaveRoom(@RequestBody LeaveRoomRequestDto request, @UserEmail String userEmail) {
+        roomService.leaveRoom(userEmail, request);
         return ApiResponse.SUCCESS;
     }
 
     @PutMapping("/disable")
-    public ApiResponse<String> disableRoom(@RequestBody DisableRoomRequestDto disableRoomRequestDto, @UserEmail String userEmail) {
-        roomService.disableRoom(userEmail, disableRoomRequestDto);
+    public ApiResponse<String> disableRoom(@RequestBody DisableRoomRequestDto request, @UserEmail String userEmail) {
+        roomService.disableRoom(userEmail, request);
         return ApiResponse.SUCCESS;
     }
 
     @DeleteMapping("/participate/force")
-    public ApiResponse<String> deleteUserForce(@RequestBody DeleteUserForceRequestDto deleteUserForceRequestDto, @UserEmail String userEmail) {
-        roomService.deleteUserForce(userEmail, deleteUserForceRequestDto);
+    public ApiResponse<String> deleteUserForce(@RequestBody DeleteUserForceRequestDto request, @UserEmail String userEmail) {
+        roomService.deleteUserForce(userEmail, request);
         return ApiResponse.SUCCESS;
     }
 
     @PutMapping("/password")
-    public ApiResponse<String> modifyRoomPassword(@RequestBody @Valid ModifyRoomPasswordRequestDto modifyRoomPasswordRequestDto, @UserEmail String userEmail) {
-        roomService.modifyRoomPassword(userEmail, modifyRoomPasswordRequestDto);
+    public ApiResponse<String> modifyRoomPassword(@RequestBody @Valid ModifyRoomPasswordRequestDto request, @UserEmail String userEmail) {
+        roomService.modifyRoomPassword(userEmail, request);
         return ApiResponse.SUCCESS;
     }
 
     @PutMapping("/owner")
-    public ApiResponse<String> delegateOwner(@RequestBody @Valid DelegateOwnerRequestDto delegateOwnerRequestDto, @UserEmail String userEmail) {
-        roomService.delegateOwner(userEmail, delegateOwnerRequestDto);
+    public ApiResponse<String> delegateOwner(@RequestBody @Valid DelegateOwnerRequestDto request, @UserEmail String userEmail) {
+        roomService.delegateOwner(userEmail, request);
         return ApiResponse.SUCCESS;
     }
 }
