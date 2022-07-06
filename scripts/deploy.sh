7e6387b973c06c34e0e6ef3d1ec5e02b6e542e53
HyeokJoon Kong <orijoon98@gmail.com>
2022-07-06 13:55:39 +0900

    [CHORE] CD를 위한 파일 수정, 추가

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+BUILD_JAR=$(ls /home/ubuntu/photory/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/photory/deploy.log
+
+echo "> build 파일 복사" >> /home/ubuntu/photory/deploy.log
+DEPLOY_PATH=/home/ubuntu/photory/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/photory/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/photory/deploy.log
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+
+echo $DEPLOY_JAR >> /home/ubuntu/photory/deploy.log
+echo $DEPLOY_PATH >> /home/ubuntu/photory/deploy.log
+echo $BUILD_JAR >> /home/ubuntu/photory/deploy.log
+echo JAR_NAME >> /home/ubuntu/photory/deploy.log
+
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/photory/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/photory/deploy.log 2>/home/ubuntu/photory/deploy_err.log &
